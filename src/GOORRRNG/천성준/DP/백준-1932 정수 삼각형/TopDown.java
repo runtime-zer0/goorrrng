627785cc07329b7bd051285981f21bd8653348d2
SeongJunChun <csj991210@naver.com>
2024-04-15 22:55:18 +0900

    Solve:행렬 곱셈 순서(DP)

--- src/GOORRRNG/천성준/DP/백준-1932 정수 삼각형/TopDown.java
+++ src/GOORRRNG/천성준/DP/백준-1932 정수 삼각형/TopDown.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-public class TopDown {
+public class Main {
 	static int[][] dp;
 
 	public static int solution(int[][] triangle) {
