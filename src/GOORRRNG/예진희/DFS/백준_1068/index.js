c8ebf736e0e61e5fd6a74a7d0364148a4e219250
sora <yejh00426@gmail.com>
2024-05-09 01:01:52 +0900

    solved : 트리 (DFS)

--- src/GOORRRNG/예진희/DFS/백준_1068/index.js
+++ src/GOORRRNG/예진희/DFS/백준_1068/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
+// 수정
 
 input.shift();
 const parentInfo = input.shift().split(' ').map(Number);
